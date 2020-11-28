a264dbb2300d07135b003a1d463d6ab471cf15e9
sharaC <sarita.sh@hotmail.com>
2020-11-28 18:14:14 -0500

    inicia proyecto

---
+++ back/src/startup/config.js
@@ -0,0 +1,11 @@
+const config = {
+    appPort: 3030,
+    HOST: 'localhost',
+    DATABASE: 'datawarehouse',
+    USER_DATABASE: 'root',
+    PASSWORD_DATABASE: 'querty123',
+    apiVersion: "/v1",
+    SECRET: 'DATAWAREHOUSE-SHARAC'
+}
+
+module.exports = config;

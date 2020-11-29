4867bacb175ff4a11d152aa978ec56fd070813f9
sharaC <sarita.sh@hotmail.com>
2020-11-28 22:16:32 -0500

    base endpoints contactos

--- Backend/src/components/contactos/controller.js
+++ Backend/src/components/contactos/controller.js
@@ -1,11 +1,11 @@
 const conexion = require('../../startup/conexion');
 
 const listarContactos = (req, res) => {
-    conexion.query(`SELECT * FROM productos;`,
+    conexion.query(`SELECT * FROM contactos;`,
     {
         type: conexion.QueryTypes.SELECT
     }).then(result => {
-        result.length === 0 ? res.status(404).json("no se encontró ningún producto") : res.status(200).json(result);
+        result.length === 0 ? res.status(404).json("no se encontró ningún contacto") : res.status(200).json(result);
     }).catch(err => {
         res.status(500).json(err);
     });
@@ -13,75 +13,59 @@ const listarContactos = (req, res) => {
 
 const listarContactoID = (req, res) => {
     let id = req.params.id;
-    conexion.query(`SELECT * FROM productos WHERE id= ?`,
+    conexion.query(`SELECT * FROM contactos WHERE id= ?`,
     {
         replacements: [id],
         type: conexion.QueryTypes.SELECT
     }).then(result => {
-        result.length === 0 ? res.status(404).json("no se encontró el producto solicitado") : res.status(200).json(result);
+        result.length === 0 ? res.status(404).json("no se encontró el contacto solicitado") : res.status(200).json(result);
     }).catch(err => {
         res.status(500).json(err);
     });
 }
 
 const crearContacto = (req, res) => {
-    let id_rol = req.id_rol;
-    if (id_rol === 1){
-        let {nombre,precio,nombre_corto,ruta,favorito} = req.body;
-        conexion.query("INSERT INTO `productos` (`nombre`,`precio`,`nombre_corto`,`ruta`,`favorito`) VALUES (?,?,?,?,?);",
-        {
-            replacements: [nombre,precio,nombre_corto,ruta,favorito],
-            type: conexion.QueryTypes.INSERT
-        }).then(result => {
-            res.status(200).json("Producto creado con id: " + result[0]);
-        }).catch(err => {
-            res.status(500).json(err);
-        });
-    } else {
-        res.status(403).json("el usuario no tiene permisos para crear nuevos productos")
-    }
+    let {nombre_completo,cargo,email,id_compania,direccion,interes,id_ciudad} = req.body;
+    conexion.query("INSERT INTO `contactos` (`nombre_completo`,`cargo`,`email`,`id_compania`,`direccion`,`interes`,`id_ciudad`) VALUES (?,?,?,?,?,?,?);",
+    {
+        replacements: [nombre_completo,cargo,email,id_compania,direccion,interes,id_ciudad],
+        type: conexion.QueryTypes.INSERT
+    }).then(result => {
+        res.status(200).json("Se creó el contacto con id: " + result[0]);
+    }).catch(err => {
+        res.status(500).json(err);
+    });
 }
 
 const editarContacto = (req, res) => {
-    let id_rol = req.id_rol;
-    if (id_rol === 1){
-        let {id, nombre,precio,nombre_corto,ruta,favorito} = req.body;
-        console.log(id);
-        conexion.query(`UPDATE productos SET nombre=?, precio=?, nombre_corto=?, ruta=?, favorito=? WHERE id=?;`,
-        {
-            replacements: [nombre,precio,nombre_corto,ruta,favorito, id],
-            type: conexion.QueryTypes.UPDATE
-        }).then(result => {
-            result[1] === 0 ? res.status(400).json("los parametros enviados para actualizar no son correctos, ningún producto se actualizó") : 
-            res.status(200).json("Producto con id: "+id+" fue actualizado correctamente");
-        }).catch(err => {
-            res.status(500).json(err);
-        });
-    } else {
-        res.status(403).json("el usuario no tiene permisos para editar productos")
-    }
+    let {id, nombre_completo,cargo,email,id_compania,direccion,interes,id_ciudad} = req.body;
+    conexion.query(`UPDATE contactos SET nombre_completo=?, cargo=?, email=?, id_compania=?, direccion=?, interes=?, id_ciudad=? WHERE id=?;`,
+    {
+        replacements: [nombre_completo,cargo,email,id_compania,direccion,interes,id_ciudad, id],
+        type: conexion.QueryTypes.UPDATE
+    }).then(result => {
+        result[1] === 0 ? res.status(400).json("los parametros enviados para actualizar no son correctos, ningún contacto se actualizó") : 
+        res.status(200).json("El contacto con id: "+id+" fue actualizado correctamente");
+    }).catch(err => {
+        res.status(500).json(err);
+    });
 }
 
 const eliminarContacto = (req, res) => {
     let id = req.params.id;
-    let id_rol = req.id_rol;
-    if (id_rol === 1){
-        conexion.query(`DELETE from productos WHERE id=${id};`,
+    conexion.query(`DELETE from contactos WHERE id=${id};`,
         {
             type: conexion.QueryTypes.UPDATE
         }).then(result => {
-            result[1] === 0 ? res.status(400).json("los parametros enviados para eliminar no son correctos, ningún producto se eliminó") :
-            res.status(200).json("Producto con id: "+id+" fue ELIMINADO correctamente");
+            result[1] === 0 ? res.status(400).json("los parametros enviados para eliminar no son correctos, ningún contacto se eliminó") :
+            res.status(200).json("El contacto con id: "+id+" fue ELIMINADO correctamente");
         }).catch(err => {
             if (err.parent.errno === 1451) {
-                res.status(409).json("El producto no puede ser eliminado por que está relacionado con un pedido en la tabla detalles_pedidos");
+                res.status(409).json("El contacto no puede ser eliminado por que está relacionado con un pedido en la tabla detalles_pedidos");
             }else{
               res.status(500).json(err);
             }
         });
-    } else {
-        res.status(403).json("el usuario no tiene permisos para eliminar productos")
-    }
     
 }
 
